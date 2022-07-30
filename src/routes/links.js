f09c8461998185ca24006e8de84c4b3c9b1dba68
LagartoRex <rios.hector.martin@gmail.com>
2022-07-30 04:04:54 -0300

    Perfil OK, ver links edit en Perfil

--- src/routes/links.js
+++ src/routes/links.js
@@ -11,11 +11,10 @@ router.get("/calendario", isLoggedIn, async (req, res) => {
 
 router.get("/perfil/:id_cliente_evt", isLoggedIn, async (req, res) => {
   const { id_cliente_evt } = req.params;
-  const eventocli = await pool.query("SELECT * FROM eventos WHERE id_cliente_evt = 1");
+  const eventocli = await pool.query("SELECT * FROM eventos WHERE id_cliente_evt = ?", [id_cliente_evt]);
   console.log(eventocli)
   res.render("links/perfil.hbs", { eventocli, dataeventos: JSON.stringify(eventocli) });
 });
-
 
 router.get("/addart", isLoggedIn, (req, res) => {
   res.render("links/addart.hbs");

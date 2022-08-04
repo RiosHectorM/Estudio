c56f583babc79ff78f47042e587a2575cc31eaa0
LagartoRex <rios.hector.martin@gmail.com>
2022-08-04 08:27:54 -0300

    upload ok

--- src/keys.js
+++ src/keys.js
@@ -1,8 +1,10 @@
 module.exports={
   database:{
-    host:'localhost',
-    user:'root',
-    password:'Gladiador.01',
-    database:'estudio'
+    host:'vd5p4ac8c3rk.aws-sa-east-1-1.psdb.cloud',
+    user:'dqtyicum7lm2',
+    password:'pscale_pw_0EB-c2TyiXDTGwdAXO9hd6byMvf9X7K4YyscXoXQ3ss',
+    database:'estudio',
+    ssl: true
   }
 };
+

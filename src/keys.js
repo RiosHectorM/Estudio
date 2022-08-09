409fef0524939dfc3f145e9768528a20b5cfa5cb
LagartoRex <rios.hector.martin@gmail.com>
2022-08-09 12:10:21 -0300

    test base datos

--- src/keys.js
+++ src/keys.js
@@ -1,8 +1,8 @@
 module.exports={
   database:{
-    host:'5kz2ye6hdkys.aws-sa-east-1-1.psdb.cloud',
-    user:'p7usjbw15kza',
-    password:'pscale_pw_wcpgBqq4oIpqeXM_tCvgY2Y_7hQ9Trj-XaXRtLYvwtw',
+    host:'9p0my9ounxvd.aws-sa-east-1-1.psdb.cloud',
+    user:'36mt4gmwwrtx',
+    password:'pscale_pw_2-Yh2ApaE-s7_ZMsanf_LjrBxaPtAA6u8FMD0NqXiac',
     database:'estudio',
     ssl: true
   }

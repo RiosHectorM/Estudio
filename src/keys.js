2136da87257badea58220ad6833e835fae458fdd
LagartoRex <rios.hector.martin@gmail.com>
2022-08-10 11:10:32 -0300

    db

--- src/keys.js
+++ src/keys.js
@@ -1,8 +1,8 @@
 module.exports={
   database:{
     host:'9p0my9ounxvd.aws-sa-east-1-1.psdb.cloud',
-    user:'h6qfw8d4lb3u',
-    password:'pscale_pw_MRStrFda1-cCjuVLkJApFzYxePrkmM4wlpNgZPq28Zo',
+    user:'1bn6j5na7aum',
+    password:'pscale_pw_x-bTWrm4mT6N4_EMMwMY4-_bRDIhGNDGPkw5K3uva6g',
     database:'estudio',
     ssl: true
   }

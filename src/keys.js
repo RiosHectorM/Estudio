5ee15483daa759c32e8a97959442c8dc8af8be1e
LagartoRex <rios.hector.martin@gmail.com>
2022-08-11 08:51:10 -0300

    db ferozo

--- src/keys.js
+++ src/keys.js
@@ -1,9 +1,9 @@
 module.exports={
   database:{
-    host:'9p0my9ounxvd.aws-sa-east-1-1.psdb.cloud',
-    user:'owg8homajutk',
-    password:'pscale_pw_qLF4qG9j2WQgSF8yFK3tBG6h38dky6Oav0Hec_fkx7Q',
-    database:'estudio',
+    host:'localhost',
+    user:'c2161585_estudio',
+    password:'78RInufore',
+    database:'c2161585_estudio',
     ssl: true
   }
 };

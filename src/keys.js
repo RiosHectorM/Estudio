b6c2bf46e88471bd8a4d37304be71a759dcccfe0
LagartoRex <rios.hector.martin@gmail.com>
2022-08-12 02:36:02 -0300

    localhost y puerto

--- src/keys.js
+++ src/keys.js
@@ -1,10 +1,11 @@
 module.exports={
   database:{
-    host:'9p0my9ounxvd.aws-sa-east-1-1.psdb.cloud',
-    user:'lqnf3fuxixh3',
-    password:'pscale_pw_tAX4wcwTNrwpUpqg9hxfCe4onUOc5lqUbX82L2GO2UM',
+    host:'127.0.0.1',
+    user:'admindirectus',
+    password:'Facu2022*',
     database:'estudio',
     ssl: true
   }
 };
+
 

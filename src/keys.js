f9bcb8a3c0d5caa06a945b12453af8a660fe8739
LagartoRex <rios.hector.martin@gmail.com>
2022-08-26 07:17:58 -0300

    ok

--- src/keys.js
+++ src/keys.js
@@ -1,13 +1,8 @@
 module.exports={
   database:{
     host:'localhost',
-<<<<<<< HEAD
-    user:'admindirectus',
-    password:'Facu2022*',
-=======
     user:'root',
     password:'Gladiador.01',
->>>>>>> 5916130 (mas cambios)
     database:'estudio',
     ssl: true
   }

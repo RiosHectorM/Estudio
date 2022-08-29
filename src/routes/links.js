4ee6b05c82cec881f15d2223024eba5d06ba24e3
LagartoRex <rios.hector.martin@gmail.com>
2022-08-29 10:36:13 -0300

    finalizado

--- src/routes/links.js
+++ src/routes/links.js
@@ -8,12 +8,17 @@ router.get("/calendario", isLoggedIn, async (req, res) => {
   res.render("links/calendario.hbs", { dataeventos: JSON.stringify(listadeeventos)});
 });
 
+//MODULO DE PERFIL
+
 router.get("/perfil/:id_cliente_evt", isLoggedIn, async (req, res) => {
   const { id_cliente_evt } = req.params;
   const clienteSelected = await pool.query("SELECT * FROM cliente WHERE id_cliente = ?", [id_cliente_evt]);
   const eventocli = await pool.query("SELECT * FROM eventos WHERE id_cliente_evt = ?", [id_cliente_evt]);
+  console.log(eventocli)
   res.render("links/perfil.hbs", { clienteSel: JSON.stringify(clienteSelected), eventocli, dataeventos: JSON.stringify(eventocli) });
 });
+
+//MODULOS RUTAS EVENTOS
 
 router.post("/addevento/:id_cliente", isLoggedIn, async (req, res) => {
   const { id_cliente } = req.params;
@@ -141,16 +146,21 @@ router.post("/addevento/:id_cliente", isLoggedIn, async (req, res) => {
   };
   console.log("elemento  ",newEvento )
   console.log("cliente  ",newClienteEstado )
-
+ 
   await pool.query("INSERT INTO eventos set ?", [newEvento]);
   await pool.query("UPDATE cliente set ? WHERE id_cliente = ?", [newClienteEstado, id_cliente]);
   res.redirect("/links/calendario");
 });
 
+router.get("/evento/:id_cliente", isLoggedIn, async (req, res) => {
+  const { id_cliente } = req.params;
+  const evento_cliente = await pool.query("SELECT * FROM eventos WHERE id_cliente_evt = ?", [id_cliente]);
+  const indice = evento_cliente.length
+  console.log(evento_cliente[indice-1])
+  res.render("links/evento.hbs", { eventocli: evento_cliente[indice-1] });
+});
 
-
-
-
+//MODULOS RUTAS CLIENTES
 
 router.get("/add", isLoggedIn,  function(req, res) {
   if(req.user) {
@@ -161,14 +171,6 @@ router.get("/add", isLoggedIn,  function(req, res) {
 } else {
   res.redirect('../../login');
 }
-});
-
-router.get("/evento/:id_cliente", isLoggedIn, async (req, res) => {
-  const { id_cliente } = req.params;
-  const evento_cliente = await pool.query("SELECT * FROM eventos WHERE id_cliente_evt = ?", [id_cliente]);
-  const indice = evento_cliente.length
-  console.log(evento_cliente[indice-1])
-  res.render("links/evento.hbs", { eventocli: evento_cliente[indice-1] });
 });
 
 router.post("/add", isLoggedIn, async (req, res) => {
@@ -189,7 +191,7 @@ try {
     turno_empresa, 
     horario_inicio,
     horario_fin, 
-    art, 
+    nombre_art, 
     prestador,  
     historia_clinica, 
     estudios_medicos, 
@@ -242,7 +244,7 @@ try {
     turno_empresa, 
     horario_inicio,
     horario_fin, 
-    art, 
+    art: nombre_art,
     prestador,  
     historia_clinica, 
     estudios_medicos, 
@@ -323,40 +325,63 @@ try {
   
 });
 
-router.get("/", isLoggedIn, async (req, res) => {
-  const links = await pool.query("SELECT * FROM cliente");
-  res.render("links/list.hbs", { links, dataCliArray: JSON.stringify(links)});
-});
-
 router.get("/edit/:id", isLoggedIn, async (req, res) => {
   const { id } = req.params;
   const links = await pool.query("SELECT * FROM cliente WHERE id_cliente = ?", [id]);
-  res.render("links/edit.hbs", { link: links[0] });
+  const artresult = await pool.query("SELECT * FROM art")
+  console.log(links[0])
+  res.render("links/edit.hbs", { link: links[0], data: JSON.stringify(artresult) });
 });
 
 router.post("/edit/:id", isLoggedIn, async (req, res) => {
   const { id } = req.params;
-  const { damnificado, domicilio, telefono } = req.body;
-  const newLink = {
+  const { 
+    fecha_ingreso,
     damnificado,
-    domicilio,
-    telefono,
+    domicilio,  
+    dni_cuil,  
+    edad, 
+    telefono, 
+    empresa, 
+    domicilio_empresa, 
+    cuit_empresa,  
+    rubro_empresa, 
+    tareas_empresa,
+    turno_empresa, 
+    horario_inicio,
+    horario_fin, 
+    nombre_art, 
+    prestador,  
+    historia_clinica, 
+    estudios_medicos, 
+    lugar_estudios 
+  } = req.body;
+  const newLink = {
+    fecha_ingreso,
+    damnificado,
+    domicilio,  
+    dni_cuil,  
+    edad, 
+    telefono, 
+    empresa, 
+    domicilio_empresa, 
+    cuit_empresa,  
+    rubro_empresa, 
+    tareas_empresa,
+    turno_empresa, 
+    horario_inicio,
+    horario_fin, 
+    art: nombre_art, 
+    prestador,  
+    historia_clinica, 
+    estudios_medicos, 
+    lugar_estudios 
   };
-  await pool.query("UPDATE links set ? WHERE id = ?", [newLink, id]);
-  req.flash("success", "Link Editado correctamente");
+  await pool.query("UPDATE cliente SET ? WHERE id_cliente = ?", [newLink, id]);
   res.redirect("/links");
 });
 
-router.get("/check/:id", isLoggedIn, async (req, res) => {
-  console.log("entro");
-  const { id } = 2;
-  const { check } = true;
-  const newCheck = {
-    check,
-  };
-  await pool.query("UPDATE eventos set ? WHERE id_cliente_evt = ?", [newCheck, id]);
-  req.flash("success", "Link Editado correctamente");
-});
+//MODULOS DE RUTAS ART
 
 router.get("/addart", isLoggedIn, (req, res) => {
   res.render("links/addart.hbs");
@@ -412,5 +437,22 @@ router.post("/editart/:id", isLoggedIn, async (req, res) => {
   res.redirect("/links/art");
 });
 
+// MODULO CHECK
+router.get("/check/:id", isLoggedIn, async (req, res) => {
+  console.log("entro");
+  const { id } = 2;
+  const { check } = true;
+  const newCheck = {
+    check,
+  };
+  await pool.query("UPDATE eventos set ? WHERE id_cliente_evt = ?", [newCheck, id]);
+  req.flash("success", "Link Editado correctamente");
+});
+
+// RUTA INICIAL
+router.get("/", isLoggedIn, async (req, res) => {
+  const links = await pool.query("SELECT * FROM cliente");
+  res.render("links/list.hbs", { links, dataCliArray: JSON.stringify(links)});
+});
 
 module.exports = router;

19d14168785c9b9ee3758639b8da366941f2412d
LagartoRex <rios.hector.martin@gmail.com>
2022-09-02 19:54:53 -0300

    no console log

--- src/routes/links.js
+++ src/routes/links.js
@@ -14,8 +14,6 @@ router.get("/perfil/:id_cliente_evt", isLoggedIn, async (req, res) => {
   const { id_cliente_evt } = req.params;
   const clienteSelected = await pool.query("SELECT * FROM cliente WHERE id_cliente = ?", [id_cliente_evt]);
   const eventocli = await pool.query("SELECT * FROM eventos WHERE id_cliente_evt = ?", [id_cliente_evt]);
-  console.log(eventocli)
-  console.log(JSON.stringify(clienteSelected))
 
   res.render("links/perfil.hbs", { clienteSelected, clienteSel: JSON.stringify(clienteSelected), eventocli, dataeventos: JSON.stringify(eventocli) });
 });
@@ -146,8 +144,6 @@ router.post("/addevento/:id_cliente", isLoggedIn, async (req, res) => {
     notas,
     fechaTeams,
   };
-  console.log("elemento  ",newEvento )
-  console.log("cliente  ",newClienteEstado )
  
   await pool.query("INSERT INTO eventos set ?", [newEvento]);
   await pool.query("UPDATE cliente set ? WHERE id_cliente = ?", [newClienteEstado, id_cliente]);
@@ -158,7 +154,6 @@ router.get("/evento/:id_cliente", isLoggedIn, async (req, res) => {
   const { id_cliente } = req.params;
   const evento_cliente = await pool.query("SELECT * FROM eventos WHERE id_cliente_evt = ?", [id_cliente]);
   const indice = evento_cliente.length
-  console.log(evento_cliente[indice-1])
   res.render("links/evento.hbs", { eventocli: evento_cliente[indice-1] });
 });
 
@@ -320,7 +315,6 @@ try {
   }
 
   await pool.query("INSERT INTO eventos set ?", [eventoInit]);
-  console.log(eventoInit)
   res.redirect("/links");
 } catch (error) {
   console.log(error)
@@ -333,7 +327,6 @@ router.get("/edit/:id", isLoggedIn, async (req, res) => {
   const { id } = req.params;
   const links = await pool.query("SELECT * FROM cliente WHERE id_cliente = ?", [id]);
   const artresult = await pool.query("SELECT * FROM art")
-  console.log(links[0])
   res.render("links/edit.hbs", { link: links[0], data: JSON.stringify(artresult) });
 });
 
@@ -445,7 +438,6 @@ router.post("/editart/:id", isLoggedIn, async (req, res) => {
 
 // MODULO CHECK
 router.get("/check/:id", isLoggedIn, async (req, res) => {
-  console.log("entro");
   const { id } = 2;
   const { check } = true;
   const newCheck = {
